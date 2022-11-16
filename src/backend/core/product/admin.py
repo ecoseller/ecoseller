6ce268f40494b9908996ee24851a40ed335ea760
Michal Půlpán <me@michalpulpan.eu>
2022-11-16 15:48:54 +0100

    [T-16] Product attributes models and admin registration of models

--- src/backend/core/product/admin.py
+++ src/backend/core/product/admin.py
@@ -1,3 +1,52 @@
 from django.contrib import admin
+from product.models import *
+from parler.admin import TranslatableAdmin, TranslatableModelForm, TranslatableTabularInline
 
-# Register your models here.
+@admin.register(ProductVariant)
+class ProductVariantAdmin(admin.ModelAdmin):
+    list_display = ('sku', 'ean', 'weight', 'update_at', 'create_at', )
+    list_filter = ('update_at', 'create_at', )
+    search_fields = ('sku', 'ean',)
+    filter_horizontal = ('attributes',)
+
+# class ProductVariantInline(admin.TabularInline):
+#     model = Product.product_variants.through
+#     extra = 0
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'category', 'published', 'update_at', 'create_at')
+    list_filter = ('published', 'category', 'update_at', 'create_at')
+    search_fields = ('translations__title', 'translations__meta_title', 'translations__meta_description', 'translations__short_description', 'translations__description', 'translations__slug')
+    filter_horizontal = ('product_variants',)
+    # inlines = [ProductVariantInline,]
+
+    # def get_prepopulated_fields(self, request, obj=None):
+    #     # can't use `prepopulated_fields = ..` because it breaks the admin validation
+    #     # for translated fields. This is the official django-parler workaround.
+    #     return {
+    #         'slug': ('title',)
+    #     }
+
+@admin.register(AttributeType)
+class AttributeTypeAdmin(admin.ModelAdmin):
+    list_display = ('type_name', 'unit')
+    search_fields = ('type_name', 'unit')
+
+@admin.register(BaseAttribute)
+class BaseAttributeAdmin(admin.ModelAdmin):
+    list_display = ('type', 'value', 'order')
+    list_filter = ('type',)
+    search_fields = ('type__type_name', 'value')
+    filter_horizontal=('ext_attributes',)
+
+@admin.register(ExtAttributeType)
+class ExtAttributeTypeAdmin(admin.ModelAdmin):
+    list_display = ('type_name', 'unit')
+    search_fields = ('type_name', 'unit')
+
+@admin.register(ExtensionAttribute)
+class ExtensionAttributeAdmin(admin.ModelAdmin):
+    list_display = ('type', 'value',)
+    list_filter = ('type',)
+    search_fields = ('type__type_name', 'value')
+    filter_horizontal= ('ext_attributes',)
--- src/backend/core/product/views.py
+++ src/backend/core/product/views.py
@@ -1,6 +1,8 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
+from .models import ProductVariant, Product, AttributeType, BaseAttribute
+
 
 class ProductDetail(APIView):
     def get(self, request, id):
