060e546a9bf9bf1d6d80e8efc980781cd6c6f9fb
Michal Půlpán <me@michalpulpan.eu>
2022-11-17 12:10:31 +0100

    translatable admin not yet working...

--- src/backend/core/product/admin.py
+++ src/backend/core/product/admin.py
@@ -8,22 +8,22 @@ class ProductVariantAdmin(admin.ModelAdmin):
     list_filter = ('update_at', 'create_at', )
     search_fields = ('sku', 'ean',)
 
-class ProductVariantInline(admin.TabularInline):
-    model = Product.product_variants.through
-    extra = 0
+# class ProductVariantInline(admin.TabularInline):
+#     model = Product.product_variants.through
+#     extra = 0
 @admin.register(Product)
-class ProductAdmin(TranslatableAdmin):
+class ProductAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'category', 'published', 'update_at', 'create_at')
     list_filter = ('published', 'category', 'update_at', 'create_at')
     search_fields = ('translations__title', 'translations__meta_title', 'translations__meta_description', 'translations__short_description', 'translations__description', 'translations__slug')
     filter_horizontal = ('product_variants',)
-    inlines = [ProductVariantInline,]
+    # inlines = [ProductVariantInline,]
 
-    def get_prepopulated_fields(self, request, obj=None):
-        # can't use `prepopulated_fields = ..` because it breaks the admin validation
-        # for translated fields. This is the official django-parler workaround.
-        return {
-            'slug': ('title',)
-        }
+    # def get_prepopulated_fields(self, request, obj=None):
+    #     # can't use `prepopulated_fields = ..` because it breaks the admin validation
+    #     # for translated fields. This is the official django-parler workaround.
+    #     return {
+    #         'slug': ('title',)
+    #     }
 
 
