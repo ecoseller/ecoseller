524cc4d467b9830650e81979e2fffc3154d5cd24
Vojtěch Lengál <vojta.lengal@gmail.com>
2022-11-18 17:50:37 +0100

    added prettier to dashboard + formatting

--- src/dashboard/pages/index.tsx
+++ src/dashboard/pages/index.tsx
@@ -1,6 +1,6 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
@@ -12,9 +12,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          ecoseller dashboard
-        </h1>
+        <h1 className={styles.title}>ecoseller dashboard</h1>
       </main>
 
       <footer className={styles.footer}>
@@ -23,12 +21,12 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
       </footer>
     </div>
-  )
+  );
 }
