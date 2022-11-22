b5ee2b21de91c2f493d0cf26f90633b5581305d1
Michal Půlpán <34132752+michalpulpan@users.noreply.github.com>
2022-11-22 17:07:03 +0100

    Revert "T-19 NextJS dashboard login page + pipeline issues on storefront"
    This reverts commit 7b096998394adeeca8579e32be907f7741228a2f.

--- src/dashboard/app/(dashboard)/page.tsx
+++
@@ -1,9 +0,0 @@
-// app/login/(dashboard)/layout.tsx
-
-export default function DashboardLayout({
-  children, // will be a page or nested layout
-}: {
-  children: React.ReactNode;
-}) {
-  return { children };
-}
--- src/storefront/app/page.tsx
+++
@@ -1,7 +0,0 @@
-// app/page.tsx
-
-const Page = () => {
-  return <h1>Storefront</h1>;
-};
-
-export default Page;
