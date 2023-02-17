cf4c1495625305b8d0dcc362051129d86240e319
Peter Lakatos <61156764+lakatop@users.noreply.github.com>
2023-02-17 17:02:59 +0100

    [T-83] added remote debugging with docker for vscode (#84)

--- src/backend/core/core/settings.py
+++ src/backend/core/core/settings.py
@@ -124,12 +124,12 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.SessionAuthentication",
         "rest_framework.authentication.BasicAuthentication",
-        "rest_framework.authentication.TokenAuthentication",
+        # "rest_framework.authentication.TokenAuthentication",
+        # "rest_framework_simplejwt.authentication.JWTAuthentication",
     ],
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.AllowAny",
-        "rest_framework_simplejwt.authentication.JWTAuthentication",
-        "rest_framework.permissions.IsAuthenticated",
+        # "rest_framework.permissions.IsAuthenticated",
     ],
 }
 
--- src/backend/core/manage.py
+++ src/backend/core/manage.py
@@ -2,11 +2,20 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from django.conf import settings
 
 
 def main():
     """Run administrative tasks."""
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
+
+    if settings.DEBUG:
+        if os.environ.get("RUN_MAIN") or os.environ.get("WERKZEUG_RUN_MAIN"):
+            import debugpy
+
+            debugpy.listen(("0.0.0.0", 8001))
+            print("Attached!")
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
