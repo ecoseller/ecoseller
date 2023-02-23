7fa48c1ecd88e46ed3e764f6a19f82494ae29250
Michal Půlpán <me@michalpulpan.eu>
2023-02-23 21:17:10 +0100

    modularizeImports for storefront and dashboard

--- src/dashboard/next.config.js
+++ src/dashboard/next.config.js
@@ -5,6 +5,32 @@ const nextConfig = {
   experimental: {
     appDir: false,
   },
+  modularizeImports: {
+    "react-bootstrap": {
+      transform: "react-bootstrap/{{member}}",
+    },
+    lodash: {
+      transform: "lodash/{{member}}",
+    },
+    "@mui/material": {
+      transform: "@mui/material/{{member}}",
+    },
+    "@mui/lab": {
+      transform: "@mui/lab/{{member}}",
+    },
+    "@mui/icons-material/?(((\\w*)?/?)*)": {
+      transform: "@mui/icons-material/{{ matches.[1] }}/{{member}}",
+    },
+  },
+  serverRuntimeConfig: {
+    // Will only be available on the server side
+    apiUrl: process.env.BACKEND_API || "http://127.0.0.1:8000",
+  },
+  publicRuntimeConfig: {
+    // Will only be available on the server side
+    apiUrl: process.env.NEXT_PUBLIC_BACKEND_API || "http://localhost:8000",
+  },
+  // output: "standalone",
 };
 
 module.exports = nextConfig;
