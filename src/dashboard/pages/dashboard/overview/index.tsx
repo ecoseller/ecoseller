1afb3af1cdf070c8ac7e349be5b208b5b16f0b5d
Michal Půlpán <me@michalpulpan.eu>
2023-03-02 13:01:58 +0100

    charts and widgets

--- src/dashboard/pages/dashboard/overview/index.tsx
+++ src/dashboard/pages/dashboard/overview/index.tsx
@@ -5,19 +5,160 @@ import DashboardLayout from "@/pages/dashboard/layout";
 //react
 import { ReactElement } from "react";
 import RootLayout from "@/pages/layout";
-// components
-
 // mui
+import ShoppingBagIcon from "@mui/icons-material/ShoppingBag";
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
+import Grid from "@mui/material/Grid";
+// components
+import SummaryWidget from "@/components/Dashboard/Overview/SummaryWidget/SummaryWidget";
+import SummaryWidgetGraphOrders from "@/components/Dashboard/Overview/SummaryWidget/SummaryWidgetGraphOrders";
 
 const DashboardOverviewPage = () => {
   return (
     <DashboardLayout>
       <Container maxWidth="xl">
         <Typography variant="h4" sx={{ mb: 5 }}>
-          Hi, John.
+          Today's order overview
         </Typography>
+        <Grid container spacing={3}>
+          <Grid item xs={12} sm={6} md={3}>
+            <SummaryWidget
+              title="Orders today"
+              value={37}
+              unit={""}
+              color="info"
+              icon={<ShoppingBagIcon sx={{ fontSize: 40 }} />}
+            />
+          </Grid>
+
+          <Grid item xs={12} sm={6} md={3}>
+            <SummaryWidget
+              title="Revenue today"
+              value={1850}
+              unit={"€"}
+              color="info"
+              icon={<ShoppingBagIcon sx={{ fontSize: 40 }} />}
+            />
+          </Grid>
+
+          <Grid item xs={12} sm={6} md={3}>
+            <SummaryWidget
+              title="Average order value"
+              value={50}
+              unit={"€"}
+              color="info"
+              icon={<ShoppingBagIcon sx={{ fontSize: 40 }} />}
+            />
+          </Grid>
+
+          <Grid item xs={12} sm={6} md={3}>
+            <SummaryWidget
+              title="Completed orders"
+              value={3}
+              unit={"%"}
+              color="info"
+              icon={<ShoppingBagIcon sx={{ fontSize: 40 }} />}
+            />
+          </Grid>
+        </Grid>
+        <Grid item xs={12} md={6} lg={8} sx={{ paddingTop: 2 }}>
+          <SummaryWidgetGraphOrders
+            title={"Orders"}
+            subheader={"Blabla"}
+            chartData={[
+              {
+                name: "Desktops",
+                data: [10, 41, 35, 51, 49, 62, 69, 91, 148],
+              },
+            ]}
+            chartOptions={{
+              chart: {
+                height: 350,
+                width: 200,
+                type: "line",
+                zoom: {
+                  enabled: false,
+                },
+              },
+              dataLabels: {
+                enabled: false,
+              },
+              stroke: {
+                curve: "straight",
+              },
+              title: {
+                text: "Product Trends by Month",
+                align: "left",
+              },
+              grid: {
+                row: {
+                  colors: ["#f3f3f3", "transparent"], // takes an array which will be repeated on columns
+                  opacity: 0.5,
+                },
+              },
+              xaxis: {
+                categories: [
+                  "Jan",
+                  "Feb",
+                  "Mar",
+                  "Apr",
+                  "May",
+                  "Jun",
+                  "Jul",
+                  "Aug",
+                  "Sep",
+                ],
+              },
+            }}
+          />
+        </Grid>
+      </Container>
+      <Container maxWidth="xl" sx={{ paddingTop: 8 }}>
+        <Typography variant="h4" sx={{ mb: 5 }}>
+          Today's recommender overview
+        </Typography>
+        <Grid container spacing={3}>
+          <Grid item xs={12} sm={6} md={3}>
+            <SummaryWidget
+              title="Orders today"
+              value={37}
+              unit={""}
+              color="info"
+              icon={<ShoppingBagIcon sx={{ fontSize: 40 }} />}
+            />
+          </Grid>
+
+          <Grid item xs={12} sm={6} md={3}>
+            <SummaryWidget
+              title="Revenue today"
+              value={1850}
+              unit={"€"}
+              color="info"
+              icon={<ShoppingBagIcon sx={{ fontSize: 40 }} />}
+            />
+          </Grid>
+
+          <Grid item xs={12} sm={6} md={3}>
+            <SummaryWidget
+              title="Average order value"
+              value={50}
+              unit={"€"}
+              color="info"
+              icon={<ShoppingBagIcon sx={{ fontSize: 40 }} />}
+            />
+          </Grid>
+
+          <Grid item xs={12} sm={6} md={3}>
+            <SummaryWidget
+              title="Completed orders"
+              value={3}
+              unit={"%"}
+              color="info"
+              icon={<ShoppingBagIcon sx={{ fontSize: 40 }} />}
+            />
+          </Grid>
+        </Grid>
       </Container>
     </DashboardLayout>
   );
