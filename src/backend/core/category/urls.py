1feaa78cfb07d33ffec5586e6b212d35faf348ef
Vojtěch Lengál <vojta.lengal@gmail.com>
2023-03-04 16:17:10 +0100

    added category view

--- src/backend/core/category/urls.py
+++ src/backend/core/category/urls.py
@@ -1,5 +1,7 @@
-# from django.urls import path
-# from . import views
+from . import views
+from django.urls import path
 
 
-urlpatterns = []
+urlpatterns = [
+    path("<int:id>/", views.CategoryView().as_view()),
+]
--- src/backend/core/category/views.py
+++ src/backend/core/category/views.py
@@ -1,3 +1,25 @@
-# from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import authentication, permissions
+from django.contrib.auth.models import User
 
-# Create your views here.
+
+class CategoryView(APIView):
+    """
+    View for category related operations
+    """
+
+    def get(self, request, id):
+        """
+        Get category by its id
+        """
+        return Response({"id": id})
+
+    def post(self):
+        pass
+
+    def put(self):
+        pass
+
+    def delete(self):
+        pass
