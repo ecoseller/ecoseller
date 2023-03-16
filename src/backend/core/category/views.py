bc1bdfe20933427cbe366d85a86df38e9eea62ab
Vojtěch Lengál <vojta.lengal@gmail.com>
2023-03-16 15:55:26 +0100

    linting

--- src/backend/core/category/views.py
+++ src/backend/core/category/views.py
@@ -1,10 +1,10 @@
-from rest_framework import mixins
 from rest_framework.status import HTTP_201_CREATED, HTTP_400_BAD_REQUEST
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
 from rest_framework.generics import RetrieveUpdateDestroyAPIView
-from django.contrib.auth.models import User
+
+# from django.contrib.auth.models import User
 from rest_framework.decorators import permission_classes
 from category.serializers import (
     CategorySerializer,
