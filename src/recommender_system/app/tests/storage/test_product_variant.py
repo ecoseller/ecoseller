79cdcef75a2ebade901b7548a27259a4f07ef3ce
matejscerba <m.scerba@seznam.cz>
2023-04-05 00:11:19 +0200

    [T-118] Last storage model test added

--- src/recommender_system/app/tests/storage/test_product_variant.py
+++ src/recommender_system/app/tests/storage/test_product_variant.py
@@ -7,23 +7,21 @@ from recommender_system.models.stored.attribute_type_product_type import (
     AttributeTypeProductTypeModel,
 )
 from recommender_system.models.stored.product import ProductModel
+from recommender_system.models.stored.product_product_variant import (
+    ProductProductVariantModel,
+)
 from recommender_system.models.stored.product_variant import ProductVariantModel
 from recommender_system.storage import ModelNotFoundException
 from tests.storage.tools import get_or_create_model, delete_model, default_dicts
 
 
-def delete_products(product_type_pk: int):
-    for product in ProductModel.gets(product_type_pk=product_type_pk):
-        product.delete()
-
-
-def delete_attribute_types(product_type_pk: int):
-    for atpt in AttributeTypeProductTypeModel.gets(product_type_pk=product_type_pk):
+def delete_products(product_variant_pk: str):
+    for ppv in ProductProductVariantModel.gets(product_variant_sku=product_variant_pk):
         try:
-            AttributeTypeModel.get(pk=atpt.attribute_type_pk).delete()
+            ProductModel.get(pk=ppv.product_id).delete()
         except ModelNotFoundException:
             pass
-        atpt.delete()
+        ppv.delete()
 
 
 @pytest.fixture
@@ -35,6 +33,7 @@ def clear_product_variant():
 
     yield product_variant_sku, product.pk
 
+    delete_products(product_variant_pk=product_variant_sku)
     delete_model(model_class=ProductVariantModel, pk=product_variant_sku)
     delete_model(model_class=ProductModel, pk=product.pk)
 
@@ -46,6 +45,7 @@ def create_product_variant():
 
     yield product_variant.pk
 
+    delete_products(product_variant_pk=product_variant.pk)
     delete_model(model_class=ProductVariantModel, pk=product_variant.pk)
     delete_model(model_class=ProductModel, pk=product.pk)
 
@@ -104,3 +104,22 @@ def test_product_variant_delete(create_product_variant):
 
     with pytest.raises(ModelNotFoundException):
         _ = ProductVariantModel.get(pk=product_variant_sku)
+
+
+def test_product_variant_products(create_product_variant):
+    product_variant_pk = create_product_variant
+    product_variant = ProductVariantModel.get(pk=product_variant_pk)
+
+    product_dict = default_dicts[ProductModel]
+
+    old_products = len(product_variant.products)
+
+    product = ProductModel.parse_obj(product_dict)
+    product.id = None
+    product.create()
+
+    ProductProductVariantModel(
+        product_id=product.id, product_variant_sku=product_variant.sku
+    ).create()
+
+    assert len(product_variant.products) == old_products + 1
