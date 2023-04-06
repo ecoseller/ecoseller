1e1dea5bcc56695b1b2fe3f3d757d2eb288f6ec6
Peter Lakatos <lakatopeter8@gmail.com>
2023-04-06 09:07:21 +0200

    forced commit before pull

--- src/dashboard/pages/dashboard/users-roles/index.tsx
+++ src/dashboard/pages/dashboard/users-roles/index.tsx
@@ -3,21 +3,38 @@
 // layout
 import DashboardLayout from "@/pages/dashboard/layout";
 //react
-import { ReactElement } from "react";
+import { ReactElement, useState } from "react";
 import RootLayout from "@/pages/layout";
 // components
 
 // mui
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
+import DashboardContentWithSaveFooter from "@/components/Dashboard/Generic/EditableContent";
+import { Stack } from "@mui/material";
+import TopLineWithReturn from "@/components/Dashboard/Catalog/Products/TopLineWithReturn";
 
 const DashboardRolesAndUsersPage = () => {
+  const [preventNavigation, setPreventNavigation] = useState<boolean>(false);
+
   return (
     <DashboardLayout>
       <Container maxWidth="xl">
-        <Typography variant="h4" sx={{ mb: 5 }}>
-          Roles & Users
-        </Typography>
+        <Stack>
+          <DashboardContentWithSaveFooter
+            primaryButtonTitle="" // To distinguish between create and update actions
+            preventNavigation={preventNavigation}
+            setPreventNavigation={setPreventNavigation}
+            onSave={async () => {
+            }}
+            returnPath={"/dashboard/catalog/product-types"}
+          >
+            <TopLineWithReturn
+              title={`Users and roles`}
+              returnPath={"/dashboard/catalog/product-types"}
+            />
+          </DashboardContentWithSaveFooter>
+        </Stack>
       </Container>
     </DashboardLayout>
   );
