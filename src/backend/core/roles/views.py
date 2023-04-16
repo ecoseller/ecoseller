6d1bd35beb8b709527b7479a923f56a113122535
Peter Lakatos <lakatopeter8@gmail.com>
2023-04-16 22:30:54 +0200

    formatting

--- src/backend/core/roles/views.py
+++ src/backend/core/roles/views.py
@@ -1,6 +1,5 @@
 from django.contrib.auth.models import Group
 from rest_framework import permissions
-from rest_framework.views import APIView
 from rest_framework.response import Response
 
 from rest_framework.generics import GenericAPIView
