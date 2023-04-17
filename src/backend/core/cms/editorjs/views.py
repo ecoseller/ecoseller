4f4f8e1d34821876b6d864b266ebc263a412a66e
Michal Půlpán <me@michalpulpan.eu>
2023-04-17 14:26:15 +0200

    stfu pipeline

--- src/backend/core/cms/editorjs/views.py
+++ src/backend/core/cms/editorjs/views.py
@@ -1,16 +1,11 @@
 import time
 from rest_framework.views import APIView
-from rest_framework.generics import RetrieveUpdateDestroyAPIView
-from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
 from rest_framework import permissions
 
 
 from rest_framework.parsers import (
     MultiPartParser,
-    FormParser,
-    FileUploadParser,
-    JSONParser,
 )
 import os
 from django.conf import settings
