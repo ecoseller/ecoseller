7b208b53c56afd1c34fc5d10a8e070b964d0343e
matejscerba <m.scerba@seznam.cz>
2023-04-22 18:42:50 +0100

    [T-160] Fixed

--- src/recommender_system/app/entrypoint.sh
+++ src/recommender_system/app/entrypoint.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-python3 recommender_system/scripts/migrate.py
-python3 recommender_system/scripts/run_server.py
+python3 -m recommender_system.scripts.migrate
+python3 -m recommender_system.scripts.run_server
