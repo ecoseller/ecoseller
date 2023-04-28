4f5a4f8b2f38c086bcf10d22cd3ba3c2da108a11
Vojtěch Lengál <vojta.lengal@gmail.com>
2023-04-28 22:08:45 +0200

    URLs fixed

--- src/backend/core/category/urls.py
+++ src/backend/core/category/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 
 
 urlpatterns = [
-    path("dashboard", views.CategoryViewDashboard().as_view()),
-    path("dashboard/<int:pk>", views.CategoryDetailViewDashboard.as_view()),
-    path("storefront/<int:pk>", views.CategoryTopLevelViewStorefront.as_view()),
-    path("storefront/top-level", views.CategoryTopLevelViewStorefront.as_view()),
-    path("dashboard/<int:id>/children", views.CategoryChildrenViewDashboard.as_view()),
+    path("dashboard/", views.CategoryViewDashboard().as_view()),
+    path("dashboard/<int:pk>/", views.CategoryDetailViewDashboard.as_view()),
+    path("storefront/<int:pk>/", views.CategoryTopLevelViewStorefront.as_view()),
+    path("storefront/top-level/", views.CategoryTopLevelViewStorefront.as_view()),
+    path("dashboard/<int:id>/children/", views.CategoryChildrenViewDashboard.as_view()),
 ]
