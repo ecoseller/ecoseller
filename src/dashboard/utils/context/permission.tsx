10811f89212de6264d4f9db5408a5a70946a9ed7
Peter Lakatos <lakatopeter8@gmail.com>
2023-04-30 11:28:07 +0200

    formatting

--- src/dashboard/utils/context/permission.tsx
+++ src/dashboard/utils/context/permission.tsx
@@ -3,69 +3,68 @@ import { useState, createContext, useContext, useEffect } from "react";
 import { useUser } from "./user";
 
 interface IPermissionContextProps {
-    hasPermission: boolean;
+  hasPermission: boolean;
 }
 
 interface IPermissionProviderProps {
-    children: React.ReactNode;
-    permission: string;
+  children: React.ReactNode;
+  permission: string;
 }
 
 const PermissionContext = createContext<Partial<IPermissionContextProps>>({});
 
 export const PermissionProvider = ({
-    children,
-    permission,
+  children,
+  permission,
 }: IPermissionProviderProps): JSX.Element => {
-    const [permissions, setPermissions] = useState<IPermission[]>([]);
-    const [hasPermission, setHasPermission] = useState<boolean>(false);
+  const [permissions, setPermissions] = useState<IPermission[]>([]);
+  const [hasPermission, setHasPermission] = useState<boolean>(false);
 
-    const { user, roles } = useUser();
+  const { user, roles } = useUser();
 
-    useEffect(() => {
-        getUserPermission();
-    }, [user, roles]);
+  useEffect(() => {
+    getUserPermission();
+  }, [user, roles]);
 
-    useEffect(() => {
-        checkHasPermission();
-    }, [permissions]);
+  useEffect(() => {
+    checkHasPermission();
+  }, [permissions]);
 
-    const getUserPermission = () => {
-        if (!user) return;
-        try {
-            // extract permissions
-            let permissions: IPermission[] = [];
-            roles.forEach((role: IGroup) => {
-                role.permissions.forEach((permission: IPermission) => {
-                    permissions.push(permission);
-                });
-            });
-            setPermissions(permissions);
-        }
-        catch (error) {
-            console.log(error);
-            setPermissions([]);
-        };
-    };
+  const getUserPermission = () => {
+    if (!user) return;
+    try {
+      // extract permissions
+      let permissions: IPermission[] = [];
+      roles.forEach((role: IGroup) => {
+        role.permissions.forEach((permission: IPermission) => {
+          permissions.push(permission);
+        });
+      });
+      setPermissions(permissions);
+    } catch (error) {
+      console.log(error);
+      setPermissions([]);
+    }
+  };
 
-    const checkHasPermission = async () => {
-        if (
-            permissions.filter((perm: IPermission) => perm.name === permission)
-                .length > 0
-        ) {
-            setHasPermission(true);
-        }
-    };
+  const checkHasPermission = async () => {
+    if (
+      permissions.filter((perm: IPermission) => perm.name === permission)
+        .length > 0
+    ) {
+      setHasPermission(true);
+    }
+  };
 
-    const value = {
-        hasPermission,
-    };
+  const value = {
+    hasPermission,
+  };
 
-    return (
-        <PermissionContext.Provider value={value}>
-            {children}
-        </PermissionContext.Provider>
-    );
+  return (
+    <PermissionContext.Provider value={value}>
+      {children}
+    </PermissionContext.Provider>
+  );
 };
 
 export const usePermission = (): any => useContext(PermissionContext);
