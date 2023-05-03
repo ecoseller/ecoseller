310c3e1faa5a86e12c48d25052916f2e065ef3f6
matejscerba <m.scerba@seznam.cz>
2023-05-03 14:56:31 +0100

    [T-167] API attribute renamed to make more sense

--- src/backend/core/api/recommender_system.py
+++ src/backend/core/api/recommender_system.py
@@ -6,11 +6,11 @@ from django.conf import settings
 
 class RecommenderSystemApi:
     server_url: str = settings.RS_URL
-    valid: bool = settings.RS_ENABLED
+    enabled: bool = settings.RS_ENABLED
 
     @classmethod
     def store_object(cls, data: Dict[str, Any]) -> None:
-        if cls.valid:
+        if cls.enabled:
             _ = requests.request(
                 method="POST", url=cls.server_url + "/store_object", json=data
             )
