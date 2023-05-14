357fc12bfc189a064786f0be97c9edf56d13a0d9
Peter Lakatos <lakatopeter8@gmail.com>
2023-05-14 19:19:50 +0200

    Added order model

--- src/backend/core/order/models.py
+++ src/backend/core/order/models.py
@@ -1,3 +1,17 @@
-# from django.db import models
+import uuid
+from django.db import models
 
-# Create your models here.
+from cart.models import Cart
+
+
+class Order(models.Model):
+    """
+    Object representing order of user.
+    """
+
+    order_id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    create_at = models.DateTimeField(auto_now_add=True)
+    paid = models.BooleanField(default=False)
+    cart = models.ForeignKey(
+        Cart, null=True, on_delete=models.SET_NULL, related_name="order"
+    )
