b8f2791ec6de039bd1e3d1a4297267739238b1a1
Vojtěch Lengál <vojta.lengal@gmail.com>
2023-05-15 19:02:49 +0200

    bug fixed

--- src/dashboard/pages/dashboard/catalog/categories/add.tsx
+++ src/dashboard/pages/dashboard/catalog/categories/add.tsx
@@ -5,6 +5,7 @@ import Container from "@mui/material/Container";
 import CategoryEditorWrapper from "@/components/Dashboard/Catalog/Categories/Editor/CategoryEditorWrapper";
 import { ICategoryEditable } from "@/types/category";
 import { OutputData } from "@editorjs/editorjs";
+import { PermissionProvider } from "@/utils/context/permission";
 
 const CategoryAddPage = () => {
   const emptyCategory: ICategoryEditable = {
@@ -32,13 +33,15 @@ const CategoryAddPage = () => {
 
   return (
     <DashboardLayout>
-      <Container maxWidth="xl">
-        <CategoryEditorWrapper
-          initialCategory={emptyCategory}
-          creatingNew={true}
-          title="Add category"
-        />
-      </Container>
+      <PermissionProvider allowedPermissions={["category_add_permission"]}>
+        <Container maxWidth="xl">
+          <CategoryEditorWrapper
+            initialCategory={emptyCategory}
+            creatingNew={true}
+            title="Add category"
+          />
+        </Container>
+      </PermissionProvider>
     </DashboardLayout>
   );
 };
