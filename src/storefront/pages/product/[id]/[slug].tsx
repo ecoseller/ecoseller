42a0910680aeb0ca4a99e10cc27009ddb06b485b
Michal Půlpán <me@michalpulpan.eu>
2023-05-16 12:51:27 +0200

    add to cart demo on product page

--- src/storefront/pages/product/[id]/[slug].tsx
+++ src/storefront/pages/product/[id]/[slug].tsx
@@ -1,5 +1,21 @@
+import { useCart } from "@/utils/context/cart";
+import Button from "@mui/material/Button";
+
 const ProductPage = ({}) => {
-  return <h1>Product page</h1>;
+  const { addToCart } = useCart();
+  return (
+    <>
+      <h1>Product page</h1>
+      <Button
+        onClick={() => {
+          console.log("add to cart");
+          addToCart("espresso250", 1);
+        }}
+      >
+        add to cart
+      </Button>
+    </>
+  );
 };
 
 export default ProductPage;
