01e90b0a3a5c5110aca58899c671575684b0249f
Michal Půlpán <me@michalpulpan.eu>
2023-05-16 15:26:11 +0200

    formating ofcourse

--- src/backend/core/cart/migrations/0018_cartitem_product.py
+++ src/backend/core/cart/migrations/0018_cartitem_product.py
@@ -7,14 +7,18 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('product', '0035_productprice_discount'),
-        ('cart', '0017_alter_cart_token'),
+        ("product", "0035_productprice_discount"),
+        ("cart", "0017_alter_cart_token"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='cartitem',
-            name='product',
-            field=models.ForeignKey(null=True, on_delete=django.db.models.deletion.SET_NULL, to='product.product'),
+            model_name="cartitem",
+            name="product",
+            field=models.ForeignKey(
+                null=True,
+                on_delete=django.db.models.deletion.SET_NULL,
+                to="product.product",
+            ),
         ),
     ]
