07854b1fd0b5aebda5ead92410bf9dd00ace01c4
Vojtěch Lengál <vojta.lengal@gmail.com>
2023-05-16 15:59:55 +0200

    obsolete code removed

--- src/dashboard/utils/axiosNextApiInstance.ts
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-/**
- * Axios instance calling Next.js server API
- */
-export const axiosNextApiInstance = axios.create({
-  baseURL: "http://localhost:3030/",
-  timeout: 1000,
-});
