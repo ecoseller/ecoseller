d69be03a70b0e402e0ef55d882d024877e89324f
Michal Půlpán <me@michalpulpan.eu>
2023-05-18 18:04:53 +0200

    grids

--- src/storefront/components/Forms/BasicField.tsx
+++ src/storefront/components/Forms/BasicField.tsx
@@ -1,23 +1,30 @@
 import { IValidatedInputField } from "@/types/common";
+import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import { ChangeEvent } from "react";
 
 const BasicField = ({ field }: { field: IValidatedInputField }) => {
   return (
-    <TextField
-      id="first-name"
-      label={field.label}
-      variant="outlined"
-      value={field.value}
-      onChange={(e: ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
-        field.setter(e.target.value);
-        if (field.validator && field.setIsValid) {
-          field.setIsValid(field.validator(e.target.value));
-        }
-      }}
-      error={field.isValid === false}
-      helperText={!field.isValid ? field?.errorMessage : ""}
-    />
+    <Box pt={2} pr={2}>
+      <TextField
+        id="first-name"
+        label={field.label}
+        variant="outlined"
+        value={field.value}
+        onChange={(e: ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
+          field.setter(e.target.value);
+          if (field.validator && field.setIsValid) {
+            field.setIsValid(field.validator(e.target.value));
+          }
+        }}
+        error={field.isValid === false}
+        helperText={!field.isValid ? field?.errorMessage : ""}
+        fullWidth
+        sx={{
+          display: "block",
+        }}
+      />
+    </Box>
   );
 };
 
--- src/storefront/pages/cart/step/1.tsx
+++ src/storefront/pages/cart/step/1.tsx
@@ -10,6 +10,7 @@ import { cartAPI } from "@/pages/api/cart/[token]";
 import { cartBillingInfoAPI } from "@/pages/api/cart/[token]/billing-info";
 import { cartShippingInfoAPI } from "@/pages/api/cart/[token]/shipping-info";
 import { IBillingInfo, IShippingInfo } from "@/types/cart";
+import Grid from "@mui/material/Grid";
 import { GetServerSideProps, NextApiRequest, NextApiResponse } from "next";
 import { useCallback, useEffect, useMemo, useState } from "react";
 
@@ -48,32 +49,42 @@ const CartStep1Page = ({ shippingInfo, billingInfo }: ICartStep1PageProps) => {
   console.log("shippingInfoState", shippingInfoState);
   return (
     <div className="container">
-      <ShippingInfoForm
-        first_name={shippingInfoState.first_name}
-        surname={shippingInfoState.surname}
-        email={shippingInfoState.email}
-        phone={shippingInfoState.phone}
-        additional_info={shippingInfoState.additional_info}
-        street={shippingInfoState.street}
-        city={shippingInfoState.city}
-        postal_code={shippingInfoState.postal_code}
-        country={shippingInfoState.country}
-        setIsFormValid={setValidShippingInfo}
-      />
-      Shipping is valid: {validShippingInfo ? "true" : "false"}
-      <BillingInfoForm
-        first_name={billingInfoState.first_name}
-        surname={billingInfoState.surname}
-        company_name={billingInfoState.company_name}
-        company_id={billingInfoState.company_id}
-        vat_number={billingInfoState.vat_number}
-        street={billingInfoState.street}
-        city={billingInfoState.city}
-        postal_code={billingInfoState.postal_code}
-        country={billingInfoState.country}
-        setIsFormValid={setValidBillingInfo}
-      />
-      Billing is valid: {validBillingInfo ? "true" : "false"}
+      <Grid
+        container
+        spacing={{ xs: 2, md: 3 }}
+        columns={{ xs: 4, sm: 8, md: 12 }}
+      >
+        <Grid container item xs={2} sm={4} md={4} direction="column">
+          <ShippingInfoForm
+            first_name={shippingInfoState.first_name}
+            surname={shippingInfoState.surname}
+            email={shippingInfoState.email}
+            phone={shippingInfoState.phone}
+            additional_info={shippingInfoState.additional_info}
+            street={shippingInfoState.street}
+            city={shippingInfoState.city}
+            postal_code={shippingInfoState.postal_code}
+            country={shippingInfoState.country}
+            setIsFormValid={setValidShippingInfo}
+          />
+          Shipping is valid: {validShippingInfo ? "true" : "false"}
+        </Grid>
+        <Grid container item xs={2} sm={4} md={4} direction="column">
+          <BillingInfoForm
+            first_name={billingInfoState.first_name}
+            surname={billingInfoState.surname}
+            company_name={billingInfoState.company_name}
+            company_id={billingInfoState.company_id}
+            vat_number={billingInfoState.vat_number}
+            street={billingInfoState.street}
+            city={billingInfoState.city}
+            postal_code={billingInfoState.postal_code}
+            country={billingInfoState.country}
+            setIsFormValid={setValidBillingInfo}
+          />
+          Billing is valid: {validBillingInfo ? "true" : "false"}
+        </Grid>
+      </Grid>
     </div>
   );
 };
