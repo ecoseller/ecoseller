73dd6e6b75c296af8b8c4f3a4bed3a825ab09f68
Vojtěch Lengál <vojta.lengal@gmail.com>
2023-05-25 15:02:04 +0200

    order grid v1

--- src/dashboard/components/Dashboard/Order/Detail/OrderDetailItemList.tsx
+++ src/dashboard/components/Dashboard/Order/Detail/OrderDetailItemList.tsx
@@ -20,85 +20,308 @@ import imgPath from "@/utils/imgPath";
 import { IOrderDetail } from "@/types/order";
 import CollapsableContentWithTitle from "../../Generic/CollapsableContentWithTitle";
 import EditorCard from "../../Generic/EditorCard";
+import {
+  DataGrid,
+  GridActionsCellItem,
+  GridColDef,
+  GridEventListener,
+  GridRowId,
+  GridRowModes,
+  GridRowModesModel,
+  GridRowParams,
+  GridRowsProp,
+  GridToolbarContainer,
+  MuiEvent,
+} from "@mui/x-data-grid";
+import { ICountry, IVatGroup } from "@/types/country";
+import { ICurrency } from "@/types/localization";
+import SaveIcon from "@mui/icons-material/Save";
+import CancelIcon from "@mui/icons-material/Cancel";
+import EditIcon from "@mui/icons-material/Edit";
+import { randomId } from "@mui/x-data-grid-generator";
+
+interface EditToolbarProps {
+  setRows: (newRows: (oldRows: GridRowsProp) => GridRowsProp) => void;
+  setRowModesModel: (
+    newModel: (oldModel: ICartItemRow) => ICartItemRow
+  ) => void;
+  productTypeId: number;
+}
+
+const EditToolbar = (props: EditToolbarProps) => {
+  const { setRows, setRowModesModel, productTypeId } = props;
+
+  const handleClick = () => {
+    const id = randomId();
+    setRows((oldRows) => [
+      ...oldRows,
+      { id, product_type: productTypeId, isNew: true },
+    ]);
+    setRowModesModel((oldModel) => ({
+      ...oldModel,
+      [id]: { mode: GridRowModes.Edit },
+    }));
+  };
+
+  return (
+    <GridToolbarContainer>
+      <Button color="primary" startIcon={<AddIcon />} onClick={handleClick}>
+        Add
+      </Button>
+    </GridToolbarContainer>
+  );
+};
 
 interface IOrderDetailItemListProps {
   cart: ICart;
 }
 
+interface ICartItemRow extends ICartItem {
+  isNew: boolean;
+  valid: boolean;
+}
+
 const OrderDetailItemList = ({ cart }: IOrderDetailItemListProps) => {
   const router = useRouter();
-  const [editing, setEditing] = useState(false);
+  const [rowModesModel, setRowModesModel] = useState<GridRowModesModel>({});
 
-  const updateItemQuantity = (item: ICartItem, addingOne: boolean = true) => {
-    if (!addingOne && item.quantity == 1) {
-      return;
-    }
+  const [rows, setRows] = useState<ICartItemRow[]>(
+    cart.cart_items.map((ci) => ({
+      ...ci,
+      isNew: false,
+      valid: true,
+    }))
+  );
 
-    const diff = addingOne ? 1 : -1;
-    // updateQuantity(item.product_variant_sku, item.quantity + diff);
+  const handleRowModesModelChange = (newRowModesModel: GridRowModesModel) => {
+    setRowModesModel(newRowModesModel);
   };
+
+  const processRowUpdate = (newRow: ICartItemRow, oldRow: ICartItemRow) => {
+    return newRow;
+    // if (!validateRow(newRow)) {
+    //   throw new Error("Invalid row");
+    // }
+    //
+    // const updatedRow = { ...newRow, isNew: false };
+    // console.log("updatedRow", updatedRow);
+    //
+    // // if row is new, save it to the database
+    // if (newRow.isNew) {
+    //   console.log("newRow", newRow);
+    //   fetch(`/api/cart/payment-method/${paymentMethod.id}/country`, {
+    //     method: "POST",
+    //     body: JSON.stringify({
+    //       payment_method: paymentMethod.id,
+    //       country: newRow.country,
+    //       vat_group: newRow.vat_group,
+    //       currency: newRow.currency,
+    //       price: newRow.price,
+    //       is_active: newRow.is_active,
+    //     }),
+    //   })
+    //     .then((res) => res.json())
+    //     .then((data) => {
+    //       setSnackbar({
+    //         open: true,
+    //         message: "Variant created",
+    //         severity: "success",
+    //       });
+    //       // get id from response and update row,
+    //       console.log("data", data);
+    //       if (data.error) throw new Error(data.error);
+    //       const { id } = data;
+    //       // remove row with newRow.id from rows and add row with id from response
+    //       setRows((rows) => [
+    //         ...rows.filter((row) => row.id !== newRow.id),
+    //         { ...updatedRow, id },
+    //       ]);
+    //     });
+    //   return updatedRow;
+    // }
+    // // if row is not new, update it in the database and update the row in the grid
+    // fetch(
+    //   `/api/cart/payment-method/${paymentMethod.id}/country/${newRow.id}/`,
+    //   {
+    //     method: "PUT",
+    //     body: JSON.stringify({
+    //       payment_method: paymentMethod.id,
+    //       country: newRow.country,
+    //       vat_group: newRow.vat_group,
+    //       currency: newRow.currency,
+    //       price: newRow.price,
+    //       is_active: newRow.is_active,
+    //     }),
+    //   }
+    // )
+    //   .then((res) => res.json())
+    //   .then(() => {
+    //     setSnackbar({
+    //       open: true,
+    //       message: "Variant updated",
+    //       severity: "success",
+    //     });
+    //     // update row in the grid
+    //     setRows((rows) => [
+    //       ...rows.filter((row) => row.id !== newRow.id),
+    //       updatedRow,
+    //     ]);
+    //   });
+    //
+    // return updatedRow;
+  };
+
+  const handleRowEditStart = (
+    params: GridRowParams,
+    event: MuiEvent<React.SyntheticEvent>
+  ) => {
+    event.defaultMuiPrevented = true;
+  };
+
+  const handleRowEditStop: GridEventListener<"rowEditStop"> = (
+    params,
+    event
+  ) => {
+    event.defaultMuiPrevented = true;
+  };
+
+  const handleEditClick = (id: GridRowId) => () => {
+    const row = rows.find((row) => row.product_variant_name === id);
+    if (!row) return;
+
+    setRowModesModel({ ...rowModesModel, [id]: { mode: GridRowModes.Edit } });
+  };
+
+  const handleSaveClick = (id: GridRowId) => () => {
+    // setRowModesModel({ ...rowModesModel, [id]: { mode: GridRowModes.View } });
+  };
+
+  const handleDeleteClick = (id: GridRowId) => () => {
+    // // delete row with id
+    // fetch(`/api/cart/payment-method/${paymentMethod.id}/country/${id}`, {
+    //   method: "DELETE",
+    // })
+    //   .then((res) => res.json())
+    //   .then(() => {
+    //     setSnackbar({
+    //       open: true,
+    //       message: "Variant deleted",
+    //       severity: "success",
+    //     });
+    //     // remove row with id from rows
+    //     setRows((rows) => rows.filter((row) => row.id !== id));
+    //   });
+  };
+
+  const handleCancelClick = (id: GridRowId) => () => {
+    // setRowModesModel({
+    //   ...rowModesModel,
+    //   [id]: { mode: GridRowModes.View, ignoreModifications: true },
+    // });
+    //
+    // const editedRow = rows.find((row) => row.id === id);
+    // if (editedRow!.isNew) {
+    //   setRows(rows.filter((row) => row.id !== id));
+    // }
+  };
+
+  const columns: GridColDef[] = [
+    {
+      field: "product_variant_name",
+      headerName: "Product variant name",
+      minWidth: 200,
+    },
+    {
+      field: "product_variant_sku",
+      headerName: "SKU",
+      minWidth: 200,
+    },
+    {
+      field: "quantity",
+      headerName: "Quantity",
+      type: "number",
+    },
+    {
+      field: "total_price_net_formatted",
+      headerName: "Total net price",
+      minWidth: 150,
+    },
+    {
+      field: "actions",
+      type: "actions",
+      headerName: "Actions",
+      width: 125,
+      minWidth: 150,
+      maxWidth: 200,
+      cellClassName: "actions",
+
+      disableColumnMenu: true,
+      getActions: ({ id }) => {
+        const isInEditMode = rowModesModel[id]?.mode === GridRowModes.Edit;
+
+        if (isInEditMode) {
+          return [
+            <GridActionsCellItem
+              icon={<SaveIcon />}
+              label="Save"
+              onClick={handleSaveClick(id)}
+              key={"save"}
+            />,
+            <GridActionsCellItem
+              icon={<CancelIcon />}
+              label="Cancel"
+              className="textPrimary"
+              onClick={handleCancelClick(id)}
+              color="inherit"
+              key={"cancel"}
+            />,
+          ];
+        }
+
+        return [
+          <GridActionsCellItem
+            icon={<EditIcon />}
+            label="Edit"
+            className="textPrimary"
+            onClick={handleEditClick(id)}
+            color="inherit"
+            key={"edit"}
+          />,
+          <GridActionsCellItem
+            icon={<DeleteIcon />}
+            label="Delete"
+            onClick={handleDeleteClick(id)}
+            color="inherit"
+            key={"delete"}
+          />,
+        ];
+      },
+    },
+  ];
 
   return (
     <EditorCard>
       <CollapsableContentWithTitle title="Order items">
-        <Table sx={{ minWidth: 650 }}>
-          <TableHead>
-            <TableRow>
-              <TableCell align="center">Product variant name</TableCell>
-              <TableCell align="center">SKU</TableCell>
-              <TableCell align="center">Quantity</TableCell>
-              <TableCell align="center">Net price</TableCell>
-              <TableCell align="center"></TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {cart.cart_items.map((item) => (
-              <TableRow key={item.product_variant_sku}>
-                <TableCell align="center">
-                  <Button
-                    onClick={() =>
-                      router.push(
-                        `/dashboard/catalog/products/edit/${item.product_id}/`
-                      )
-                    }
-                  >
-                    {item.product_variant_name}
-                  </Button>
-                </TableCell>
-
-                <TableCell align="center">{item.product_variant_sku}</TableCell>
-
-                <TableCell align="center" width="50">
-                  <TextField
-                    id="quantity"
-                    type="number"
-                    InputLabelProps={{
-                      shrink: true,
-                    }}
-                    value={item.quantity}
-                    disabled={!editing}
-                  />
-                </TableCell>
-
-                <TableCell sx={{ fontWeight: 700 }} align="center">
-                  {item.total_price_net_formatted}
-                </TableCell>
-
-                <TableCell align="center">
-                  {editing ? (
-                    <IconButton
-                      onClick={() => {
-                        // removeFromCart(item.product_variant_sku);
-                      }}
-                    >
-                      <DeleteIcon />
-                    </IconButton>
-                  ) : null}
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
+        <DataGrid
+          rows={rows}
+          columns={columns}
+          editMode={"row"}
+          hideFooter={true}
+          autoHeight={true}
+          rowModesModel={rowModesModel}
+          onRowModesModelChange={handleRowModesModelChange}
+          onRowEditStart={handleRowEditStart}
+          onRowEditStop={handleRowEditStop}
+          processRowUpdate={processRowUpdate}
+          slots={{
+            toolbar: EditToolbar,
+          }}
+          slotProps={{
+            toolbar: { setRows, setRowModesModel },
+          }}
+          getRowId={(row) => row.product_variant_name}
+          sx={{ overflowX: "scroll" }}
+        />
         <Grid container justifyContent="center" sx={{ my: 3 }}>
           <Grid item>
             <Typography variant="h6">
