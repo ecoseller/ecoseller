bf0f332ee499cb79c513b1a6f785094e154aaeb8
Vojtěch Lengál <vojta.lengal@gmail.com>
2023-06-14 10:09:07 +0200

    Subcategory list done

--- src/storefront/components/Category/SubCategoryList.tsx
+++ src/storefront/components/Category/SubCategoryList.tsx
@@ -1,5 +1,11 @@
 import { ICategoryBase } from "@/types/category";
 import { useRouter } from "next/router";
+import React from "react";
+import { Grid } from "@mui/material";
+import PaperItem from "@/components/Generic/PaperItem";
+import CollapsableContentWithTitle from "@/components/Generic/CollapsableContentWithTitle";
+import NextLink from "next/link";
+import MUILink from "@mui/material/Link";
 
 interface ISubCategoryListProps {
   subCategories: ICategoryBase[];
@@ -13,11 +19,19 @@ const SubCategoryList = ({ subCategories }: ISubCategoryListProps) => {
   const router = useRouter();
 
   return (
-    <ul>
-      {subCategories.map((c) => (
-        <li>{c.title}</li>
-      ))}
-    </ul>
+    <CollapsableContentWithTitle title="Subcategories" defaultOpen={true}>
+      <Grid container spacing={{ xs: 1, sm: 2 }}>
+        {subCategories.map((c) => (
+          <Grid item xs={12} sm={6} md={4} lg={3}>
+            <PaperItem>
+              <NextLink href={`/category/${c.id}/${c.slug}`}>
+                {c.title}
+              </NextLink>
+            </PaperItem>
+          </Grid>
+        ))}
+      </Grid>
+    </CollapsableContentWithTitle>
   );
 };
 
--- src/storefront/pages/category/[id]/[slug].tsx
+++ src/storefront/pages/category/[id]/[slug].tsx
@@ -10,21 +10,32 @@ import { categoryDetailAPI } from "@/pages/api/category/[id]";
 import EditorJsOutput from "@/utils/editorjs/EditorJsOutput";
 import Typography from "@mui/material/Typography";
 import SubCategoryList from "@/components/Category/SubCategoryList";
+import HeadMeta from "@/components/Common/SEO";
+import { useRouter } from "next/router";
+import ProductList from "@/components/Category/ProductList";
 
 interface ICategoryPageProps {
   category: ICategoryDetail;
 }
 
 const CategoryPage = ({ category }: ICategoryPageProps) => {
+  const router = useRouter();
+
   return (
     <>
+      <HeadMeta
+        title={category.meta_title}
+        description={category.meta_description}
+        url={router.basePath}
+      />
       <div className="container">
         <Typography variant="h4">{category.title}</Typography>
         <EditorJsOutput data={category.description_editorjs} />
+        {category.children.length > 0 ? (
+          <SubCategoryList subCategories={category.children} />
+        ) : null}
+        <ProductList />
       </div>
-      {category.children.length > 0 ? (
-        <SubCategoryList subCategories={category.children} />
-      ) : null}
     </>
   );
 };
