aa18123d73affe50e68e54b0936d56c43c968df3
Michal Půlpán <me@michalpulpan.eu>
2023-06-16 13:56:47 +0200

    SafeDeleteModel

--- src/backend/core/product/models.py
+++ src/backend/core/product/models.py
@@ -1,3 +1,4 @@
+from core.safe_delete import SafeDeleteModel
 from ckeditor.fields import RichTextField
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
