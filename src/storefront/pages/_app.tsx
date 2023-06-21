2786ba00251f9f1bb2a7786c1500954e91783dd1
Michal Půlpán <me@michalpulpan.eu>
2023-06-21 12:49:02 +0200

    recommender context provider

--- src/storefront/pages/_app.tsx
+++ src/storefront/pages/_app.tsx
@@ -4,15 +4,18 @@ import "keen-slider/keen-slider.min.css";
 import { CartProvider } from "@/utils/context/cart";
 import { UserProvider } from "@/utils/context/user";
 import { CountryProvider } from "@/utils/context/country";
+import { RecommenderProvider } from "@/utils/context/recommender";
 
 export default function App({ Component, pageProps }: any) {
   return (
     <CountryProvider>
       <CartProvider>
         <UserProvider>
-          <RootLayout>
-            <Component {...pageProps} />
-          </RootLayout>
+          <RecommenderProvider>
+            <RootLayout>
+              <Component {...pageProps} />
+            </RootLayout>
+          </RecommenderProvider>
         </UserProvider>
       </CartProvider>
     </CountryProvider>
