a48993b615694c0bb74d6e572ef37e4cf84764f5
Michal Půlpán <me@michalpulpan.eu>
2023-06-27 12:25:04 +0200

    cms page rendering

--- src/storefront/pages/[slug].tsx
+++ src/storefront/pages/[slug].tsx
@@ -5,7 +5,11 @@
  */
 
 import { IPageCMS } from "@/types/cms";
-import { GetServerSideProps } from "next";
+import { GetServerSideProps, NextApiRequest, NextApiResponse } from "next";
+import { pageCMSAPI } from "./api/cms/[locale]/[slug]";
+import { Box, Typography } from "@mui/material";
+import { FontDownload } from "@mui/icons-material";
+import EditorJsOutput from "@/utils/editorjs/EditorJsOutput";
 
 interface CMSPageProps {
   page: IPageCMS;
@@ -13,9 +17,27 @@ interface CMSPageProps {
 
 const CMSPage = ({ page }: CMSPageProps) => {
   return (
-    <>
-      <h1>Page not found</h1>
-    </>
+    <div className="container">
+      <Typography
+        variant="h4"
+        component="h1"
+        sx={{
+          mb: 2,
+          mt: 4,
+          fontWeight: 700,
+        }}
+      >
+        {page.title}
+      </Typography>
+      <Box sx={{ pt: 5, pl: 3 }}>
+        <EditorJsOutput data={page.content} />
+      </Box>
+      <Box
+        sx={{
+          mt: 5,
+        }}
+      />
+    </div>
   );
 };
 
@@ -23,14 +45,31 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
   const { slug } = context.query;
   const { res, req, locale } = context;
   const language = Array.isArray(locale) ? locale[0] : locale || "en";
+
   // fetch page from backend
+  const page = await pageCMSAPI(
+    "GET",
+    language,
+    slug as string,
+    req as NextApiRequest,
+    res as NextApiResponse
+  )
+    .then((data) => data)
+    .catch((error) => null);
 
   // if page not found, return 404
+  if (page == undefined || page == null || page?.message == "page not found") {
+    return {
+      notFound: true,
+    };
+  }
 
-  return {
-    notFound: true,
-  };
   // if page found, return page data
+  return {
+    props: {
+      page,
+    },
+  };
 };
 
 export default CMSPage;
--- src/storefront/pages/api/cms/[locale]/[slug].tsx
+++ src/storefront/pages/api/cms/[locale]/[slug].tsx
@@ -2,9 +2,10 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import { api, setRequestResponse } from "@/utils/interceptors/api";
 import { HTTPMETHOD } from "@/types/common";
 
-export const orderSubmitAPI = async (
+export const pageCMSAPI = async (
   method: HTTPMETHOD,
-  identifier: string,
+  locale: string,
+  slug: string,
   req: NextApiRequest,
   res: NextApiResponse
 ) => {
@@ -12,10 +13,16 @@ export const orderSubmitAPI = async (
     setRequestResponse(req, res);
   }
 
-  let url = `cms/storefront/category/type/${identifier}/pages/`;
+  let url = `/cms/storefront/page/cms/${locale}/${slug}/`;
 
   if (method === "GET") {
-    return await api.get(url).then((response) => response.data);
+    return await api.get(url).then((response) => {
+      if (response.data && response.status == 200) {
+        return response.data;
+      } else {
+        throw new Error("Page not found");
+      }
+    });
   } else {
     throw new Error("Method not supported");
   }
@@ -26,10 +33,10 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
    * This is a wrapper for the CMS API in the backend to list all the categories and their pages for the footer menu.
    */
   const { method, query } = req;
-  const { identifier } = query;
+  const { locale, slug } = query;
 
   if (method == "GET") {
-    return orderSubmitAPI("GET", identifier as string, req, res)
+    return pageCMSAPI("GET", locale as string, slug as string, req, res)
       .then((data) => res.status(200).json(data))
       .catch((error) => res.status(400).json(error.response.data));
   }
