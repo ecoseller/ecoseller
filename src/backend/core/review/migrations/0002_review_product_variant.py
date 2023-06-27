aa43b2b255896cb67885588e2118b2e7b034928b
Michal Půlpán <me@michalpulpan.eu>
2023-06-27 10:51:09 +0200

    backend CMS page list preparation

--- src/backend/core/review/migrations/0002_review_product_variant.py
+++ src/backend/core/review/migrations/0002_review_product_variant.py
@@ -5,7 +5,6 @@ import django.db.models.deletion
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         ("product", "0038_attributetype_baseattribute_and_more"),
         ("review", "0001_initial"),
