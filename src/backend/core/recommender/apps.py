e5c2d3c0059a20dcba1ec83d898f8f2d9f50d3c0
Michal Půlpán <me@michalpulpan.eu>
2023-07-04 16:19:07 +0200

    formatting

--- src/backend/core/recommender/apps.py
+++ src/backend/core/recommender/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class RecommenderConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'recommender'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "recommender"
