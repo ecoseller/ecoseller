6413455c57a55e401fe24625d4e2720431c3ab41
Michal Půlpán <me@michalpulpan.eu>
2023-07-08 15:40:21 +0200

    scrollable country select

--- src/storefront/components/Common/CountrySelect.tsx
+++ src/storefront/components/Common/CountrySelect.tsx
@@ -41,7 +41,13 @@ const CountrySelect = () => {
 export default CountrySelect;
 
 const style = {
-  position: "absolute" as "absolute",
+  position: "absolute",
+  // left: "10%",
+  overflow: "scroll",
+  height: "80%",
+  display: "block",
+  // position: "absolute" as "absolute",
+  // overflow: "scroll",
   top: "50%",
   left: "50%",
   transform: "translate(-50%, -50%)",
