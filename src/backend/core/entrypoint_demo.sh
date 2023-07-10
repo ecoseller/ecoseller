51a0338d9bc84dfc752d4bb5ee1d86510d3ad70e
Michal Půlpán <me@michalpulpan.eu>
2023-07-10 21:46:32 +0200

    fixed austria locale

--- src/backend/core/entrypoint_demo.sh
+++ src/backend/core/entrypoint_demo.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
+cd /usr/src/
 git clone https://github.com/ecoseller/demo-data.git
-mv ./demo-data/ /usr/src/demo-data
+# mv ./demo-data/ /usr/src/demo-data
 mv /usr/src/demo-data/media /usr/src/mediafiles
 PGPASSWORD=$POSTGRES_PASSWORD psql -h $POSTGRES_HOST -U $POSTGRES_USER -d $POSTGRES_DB -p $POSTGRES_PORT -a -f /usr/src/demo-data/sql/mock_data.sql
