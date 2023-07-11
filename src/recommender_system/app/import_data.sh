5efa6e44f00ead8df10865d0983dd7af10e6ce58
matejscerba <m.scerba@seznam.cz>
2023-07-11 20:12:51 +0200

    [T-328] RS Feedback data added

--- src/recommender_system/app/import_data.sh
+++ src/recommender_system/app/import_data.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
+
+echo "Importing data..."
 apt-get install -y git postgresql-client
 git clone https://github.com/ecoseller/demo-data.git
 mv ./demo-data/ /usr/src/demo-data
 psql $RS_PRODUCT_DB_URL -a -f /usr/src/demo-data/sql/mock_data_rs_products.sql
+psql $RS_FEEDBACK_DB_URL -a -f /usr/src/demo-data/sql/mock_data_rs_feedback.sql
