14018efdb69a5eadf87cffc89306606a7ada02d6
Vojtěch Lengál <vojta.lengal@gmail.com>
2023-07-11 21:32:37 +0200

    bug fixed

--- src/backend/core/order/urls.py
+++ src/backend/core/order/urls.py
@@ -6,7 +6,9 @@ urlpatterns = [
     path("dashboard/", views.OrderListDashboardView.as_view()),
     path("dashboard/today-stats/", views.OrderListTodayDashboardView.as_view()),
     path("dashboard/month-stats/", views.OrderListMonthDashboardView.as_view()),
-    path("dashboard/complaint/<int:id>", views.OrderItemComplaintDashboardView.as_view()),
+    path(
+        "dashboard/complaint/<int:pk>/", views.OrderItemComplaintDashboardView.as_view()
+    ),
     path("dashboard/<str:token>/", views.OrderDetailDashboardView.as_view()),
     path("storefront/", views.OrderCreateStorefrontView.as_view()),
     path("storefront/orders/", views.OrderListStorefrontView.as_view()),
