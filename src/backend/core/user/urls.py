fa0f02218ec955c7e0b8e71820e8809d6ef7270e
Peter Lakatos <61156764+lakatop@users.noreply.github.com>
2023-07-12 15:11:46 +0200

    [H-17] Various bug fixes (#329)

--- src/backend/core/user/urls.py
+++ src/backend/core/user/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path("login", views.CustomTokenObtainPairView.as_view(), name="token_create"),
     path("logout", views.BlacklistTokenView.as_view()),
     path("refresh-token", jwt_views.TokenRefreshView.as_view(), name="token_refresh"),
-    path("users", views.UserView.as_view()),
+    path("users/", views.UserView.as_view()),
     path("users/<str:id>", views.UserDetailView.as_view()),
     path("password", views.PasswordView.as_view()),
     path("password/<str:id>", views.PasswordAdminView.as_view()),
