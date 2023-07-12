07f54ec54e86c815d88c4d9972101d7264eb031d
Vojtěch Lengál <vojta.lengal@gmail.com>
2023-07-12 18:36:51 +0200

    black

--- src/backend/core/product/migrations/0046_alter_baseattributetranslation_name.py
+++ src/backend/core/product/migrations/0046_alter_baseattributetranslation_name.py
@@ -6,13 +6,18 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('product', '0045_attributetype_safe_deleted_and_more'),
+        ("product", "0045_attributetype_safe_deleted_and_more"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='baseattributetranslation',
-            name='name',
-            field=models.CharField(blank=True, help_text='Base Attribute in given language', max_length=200, null=True),
+            model_name="baseattributetranslation",
+            name="name",
+            field=models.CharField(
+                blank=True,
+                help_text="Base Attribute in given language",
+                max_length=200,
+                null=True,
+            ),
         ),
     ]
