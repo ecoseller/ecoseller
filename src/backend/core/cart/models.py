bf46a205d03372689e7789a4d539e52d3f41289f
Vojtěch Lengál <vojta.lengal@gmail.com>
2023-07-16 22:18:27 +0200

    bugs fixed

--- src/backend/core/cart/models.py
+++ src/backend/core/cart/models.py
@@ -334,7 +334,7 @@ class Cart(models.Model):
         Recalculate cart prices.
         """
         if (self.pricelist and self.pricelist.code == pricelist.code) and (
-            self.country.code == country.code
+                self.country.code == country.code
         ):
             # if pricelist and country is the same as before, we don't need to recalculate
             return
@@ -442,6 +442,21 @@ class CartItem(models.Model):
             return self.total_price_without_vat_formatted
 
         total_price = self.unit_price_without_vat * self.quantity
+        total_price_before_discount = total_price / (1 - self.discount / 100)
+
+        return self.cart.pricelist.format_price(total_price_before_discount)
+
+    @property
+    def total_price_incl_vat_before_discount_formatted(self):
+        """
+        Get total price including VAT before discount (unit price * quantity) of this item with currency symbol
+
+        This price is intended to be shown to the user.
+        """
+        if not self.discount or (self.discount == 0):
+            return self.total_price_incl_vat_formatted
+
+        total_price = self.unit_price_incl_vat * self.quantity
         total_price_before_discount = total_price / (1 - self.discount / 100)
 
         return self.cart.pricelist.format_price(total_price_before_discount)
