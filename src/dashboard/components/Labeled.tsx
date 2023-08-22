47025d4817f73d2a0ed2c3723cb1fc9bfd3cb413
matejscerba <m.scerba@seznam.cz>
2023-08-22 12:19:01 +0200

    tooltips added

--- src/dashboard/components/Labeled.tsx
+++ src/dashboard/components/Labeled.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
 import InfoIcon from "@mui/icons-material/Info";
+import Tooltip from "@mui/material/Tooltip";
 
 interface ILabeledProps {
   label: string;
@@ -13,7 +14,15 @@ const Labeled = ({ label, description, children }: ILabeledProps) => (
   <Stack>
     <Typography variant="body1">
       {label}
-      {description !== null && description !== undefined && <InfoIcon />}
+      {description !== null && description !== undefined && (
+        <Tooltip title={description} arrow placement={"top"}>
+          <InfoIcon
+            fontSize={"inherit"}
+            color={"primary"}
+            sx={{ ml: 1, position: "relative", top: "2px" }}
+          />
+        </Tooltip>
+      )}
     </Typography>
     {children}
   </Stack>
